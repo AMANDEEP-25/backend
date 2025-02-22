6929f68ab103bed8a0cd63fec9d0fef0d76faa66
Amandeep Singh <jyotsnaaman2003@gmail.com>
2025-02-22 20:52:34 +0530

    setup completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -75,13 +75,10 @@ userSchema.methods.generateRefreshToken = function () {
   jwt.sign(
     {
       _id: this._id,
-      username: this.username,
-      fullName: this.fullName,
-      email: this.email,
     },
-    process.env.ACCESS_TOKEN_SECRET,
+    process.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
